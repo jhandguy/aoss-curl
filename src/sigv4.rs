37334bf2b6bff29524d8a5117c63a9de93da7187
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2022-10-30 22:43:16 +0100

    chore: add unit test

--- src/sigv4.rs
+++ src/sigv4.rs
@@ -15,7 +15,6 @@ pub async fn get_signed_headers(
     body: &str,
 ) -> Result<HeaderMap> {
     let now = SystemTime::now();
-
     let params = SigningParams::builder()
         .settings(SigningSettings::default())
         .time(now)
@@ -42,4 +41,38 @@ pub async fn get_signed_headers(
     .clone();
 
     Ok(headers)
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::{anyhow, Result};
+    use aws_mfa::auth::Credentials;
+    use hyper::{Body, Request};
+
+    use crate::sigv4::get_signed_headers;
+
+    #[tokio::test]
+    async fn test_get_signed_headers() -> Result<()> {
+        let credentials = Credentials::new("access_key_id", "secret_access_key", "session_token");
+        let body = "";
+        let request = Request::builder()
+            .uri("https://opensearch-domain.eu-west-1.es.amazonaws.com/_cat/indices")
+            .method("GET")
+            .body(Body::from(String::from(body)))?;
+        let headers = get_signed_headers("eu-west-1", "es", &credentials, &request, body).await?;
+
+        assert_eq!(headers.len(), 3);
+        assert!(headers.get("x-amz-date").is_some());
+        assert!(headers.get("authorization").is_some());
+        assert_eq!(
+            headers
+                .get("x-amz-security-token")
+                .ok_or_else(|| anyhow!("x-amz-security-token header missing"))?
+                .clone()
+                .to_str()?,
+            credentials.session_token()
+        );
+
+        Ok(())
+    }
 }
